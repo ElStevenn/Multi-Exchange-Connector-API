b8d24c62639b165b1fedd5cd8cfc2c32ac07ffc6
ElStevenn <sfukinguay@gmail.com>
2025-01-02 15:12:51 +0100

    solved the issue with the DNS

--- scripts/CI/unit_testing.sh
+++ scripts/CI/unit_testing.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+container_name="multiexchange_apiv2"
+
 echo "Running unit tests..."
 
-
 # Check if getting tables works
-docker cp 
+docker cp /home/ubuntu/Multi-Exchange-Connector-API/src/app/database/database.py $multiexchange_apiv2:/src/app/database/database.py
