f1d791d3e0ed7146d8f2194f47f382b96d0bc1cd
ElStevenn <sfukinguay@gmail.com>
2025-01-04 22:06:21 +0100

    fixed bug with celery

--- src/app/celery_app/celery_config.py
+++ src/app/celery_app/celery_config.py
@@ -26,7 +26,8 @@ celery_app.conf.update(
 # Celery Beat Schedule
 celery_app.conf.beat_schedule = {
     'fetch-user-assets-every-hour-on-the-hour': {
-        'task': 'src.app.celery.tasks.fetch_user_assets_concurrently',
-        'schedule': crontab(minute=0, hour='*'),  # Every hour, at minute 0 (e.g., 5:00, 6:00, etc.)
+        'task': 'app.celery_app.tasks.fetch_user_assets_concurrently',  # <--- match the decorator
+        'schedule': crontab(minute=0, hour='*'),
     },
 }
+
